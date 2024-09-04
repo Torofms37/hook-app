92ed4e290dbea7e9badce3521be5bdbfe623b140
torofms37 <torofms37@gmail.com>
2024-09-03 23:47:31 -0700

    nuevas cosas 11

---
+++ src/03-examples/Quote.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Quote = ( {author, quote} ) => {
+  return (
+    <div>Quote</div>
+  )
+}
--- src/05-useLayoutEffect/Layout.jsx
+++ src/05-useLayoutEffect/Layout.jsx
@@ -1,38 +1,20 @@
 import { useCounter, useFetch } from "../hooks";
-import { LoadingMessage } from "./LoadingMessage";
-import { PokemonCard } from "./PokemonCard";
+import { LoadingMessage, PokemonCard } from '../03-examples'
 
 export const Layout = () => {
 
   const { counter, rest, sum } = useCounter(1);
-  const {data, hasError, isLoading} = useFetch(`https://pokeapi.co/api/v2/pokemon/${ counter }`);
+  const {data, hasError, isLoading} = useFetch(`https://breakingbadapi.com/api/quotes/${ counter }`);
+  const { author, quote } = !!data && data[0];
 
   return (
     <>
-      <h1>Información del Pikemon</h1>
+      <h1>BreakingBad quotes</h1>
       <hr />
 
-      { isLoading 
-        ? 
-        <LoadingMessage /> 
-        : (
-          <PokemonCard 
-            id={counter}
-            name={data.name}
-            sprites={[
-              data.sprites.front_default,
-              data.sprites.front_shiny,
-              data.sprites.back_default,
-              data.sprites.back_shiny,
-            ]}
-          />
-          )
-      }
-
-      {/* <pre> {JSON.stringify(data, null, 2)} </pre> */}
-      
-      {/* <h2> {data?.name} </h2>
-      Si tenemos 'data?', entonces busca en '.name' */}
+    {
+      isLoading ? <LoadingMessage /> : <Quote author={ author } quote={quote} />
+    }
 
 
 
